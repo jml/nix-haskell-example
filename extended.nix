e47a6056b2981c86a5a752bccb4b9c85e103cde8
Jonathan Lange <jml@mumak.net>
2016-07-24 11:31:17 +0100

    Make hook composable

--- extended.nix
+++ extended.nix
@@ -4,9 +4,12 @@ let
   inherit (pkgs) haskell pstree;
   example = pkgs.haskellPackages.callPackage ./. {};
 
+  addRuntimeDependency = drv: x: addRuntimeDependencies drv [x];
   addRuntimeDependencies = drv: xs: haskell.lib.overrideCabal drv (drv: {
     buildDepends = (drv.buildDepends or []) ++ [ pkgs.makeWrapper ];
     postInstall = ''
+      ${drv.postInstall or ""}
+
       for exe in "$out/bin/"* ; do
         wrapProgram "$exe" --prefix PATH ":" \
           ${pkgs.lib.makeBinPath xs}
@@ -14,4 +17,4 @@ let
     '';
   });
 
-in addRuntimeDependencies example [ pstree ]
+in addRuntimeDependency example pstree
